255fbdea597f1f760d580f204261832ac2328871
ClaraLaille <55700992+ClaraLaille@users.noreply.github.com>
2019-09-23 17:09:50 +0200

    Add files via upload

---
+++ Rstudio exemple.R
@@ -0,0 +1,30 @@
+2 + 3
+8 - 12
+14 * 25
+-3/10
+4 - sqrt(2)
+x <- 2
+4 * x
+x
+y <-seq(from=0,to=10^6,by=1)
+y
+y[10000]
+x <- 2
+x = 2
+2 -> x
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+mean(tailles) #moyenne
+var(tailles) #variance
+sd(tailles) #écart-type
+summary(tailles) 
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(tailles)
+length(poids)
+tailles.m <- tailles/100
+tailles.m
+imc <- poids/taille.m^2
+imc
